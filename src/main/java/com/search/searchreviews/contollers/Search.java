921f19cd40ca381c1a7ce8fb710924983ca22711
MOHAMMED KASMI <mohammed.kasmi1@usmba.ac.ma>
2020-06-06 02:59:36 +0100

    second commit

--- src/main/java/com/search/searchreviews/contollers/Search.java
+++ src/main/java/com/search/searchreviews/contollers/Search.java
@@ -5,16 +5,11 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
+@CrossOrigin(origins = "*")
 public class Search {
 
     private RestTemplate restTemplate;
@@ -23,22 +18,31 @@ public class Search {
         return new RestTemplate();
     }
 
-    @GetMapping(path = "/reviews/{tag}/{classification}/{sort}")
+    @GetMapping(path = "/reviews")
     @ResponseBody
-    public ResponseNormalSearch reviewByTag(@PathVariable("tag")String tag,@PathVariable("classification")String classification,@PathVariable("sort")String sort){
+    public ResponseNormalSearch reviewByTag(@RequestParam("keyword")String keyword,@RequestParam("group") String group,@RequestParam("year")int year){
         restTemplate=SpringbootConsumeRestExampleApplication();
         HttpHeaders headers=new HttpHeaders();
         headers.set("Authorization","Token token=129c7c0036304e2b8c7b046200256dbd");
-        String url="https://api.epistemonikos.org/v1/documents/search?q={tag}&classification={classification}&sort={sort}&show=external_links";
+        String url="https://api.epistemonikos.org/v1/documents/search?q="+keyword+"&classification=systematic-review&show=external_links";
+
+        if(!group.equals("")){
+            url+="&group="+group;
+        }
+        if(year!=0){
+            url+="year="+year;
+        }
+        //
         HttpEntity entity=new HttpEntity(headers);
 
-        Map<String,String> params=new HashMap<>();
+        //Map<String,String> params=new HashMap<>();
 
-        params.put("tag",tag);
-        params.put("classification",classification);
-        params.put("sort",sort);
+        //params.put("q",keyword);
+        //params.put("classification","systematic-review");
+        //params.put("sort",sort);
+        //params.put("show","external_links");
 
-        ResponseEntity<ResponseNormalSearch> response=restTemplate.exchange(url, HttpMethod.GET, entity, ResponseNormalSearch.class,params);
+        ResponseEntity<ResponseNormalSearch> response=restTemplate.exchange(url, HttpMethod.GET, entity, ResponseNormalSearch.class);
         //System.out.println("total search results"+response.getBody().getSearch_info().getTotal_hits());
         return response.getBody();
     }
